ee93744b4ed1e0c7359d47ddc7bb7844e9908868
ViktorNikolaevi4 <v87v87@icloud.com>
2025-11-10 14:29:30 +0300

    Initial Commit

---
+++ NewWWViktor/NewWWViktorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWWViktorApp.swift
+//  NewWWViktor
+//
+//  Created by Виктор Корольков on 10.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct NewWWViktorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
